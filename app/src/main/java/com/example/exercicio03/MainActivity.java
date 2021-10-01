6eb1306548d88b9d99bced2e37b84bf494f6f4e4
MatheusTessaro <tessaro_matheus@hotmail.com>
2021-10-01 16:49:09 -0300

    up

--- app/src/main/java/com/example/exercicio03/MainActivity.java
+++ app/src/main/java/com/example/exercicio03/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.exercicio03;
 
 
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,6 +11,8 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -24,8 +27,10 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     private CheckBox ckbNegrito;
     private CheckBox ckbItalico;
     private CheckBox ckbMaiusculo;
+    private RadioButton rdbRoxo;
+    private RadioButton rdbLaranja;
+    private RadioButton rdbCiano;
 
-    private String[] corHex={"00","00","00"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +44,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         ckbNegrito = findViewById(R.id.ckbNegrito);
         ckbItalico = findViewById(R.id.ckbItalico);
         ckbMaiusculo = findViewById(R.id.ckbMaiusculo);
+        rdbRoxo = findViewById(R.id.rdbRoxo);
+        rdbLaranja = findViewById(R.id.rdbLaranja);
+        rdbCiano = findViewById(R.id.rdbCiano);
 
         btnEnviar.setOnClickListener(this);
         skbTamanhoLetra.setOnSeekBarChangeListener(this);
@@ -57,11 +65,14 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
     @Override
     public void onClick(View view) {
         if(view.getId() == R.id.btnEnviar) {
-            textField.setText(edtText.getText());
-            edtText.getText().clear();
+                String aux = "";
+                aux = aux.concat(textField.getText().toString() + " " + edtText.getText().toString());
+
+
+                textField.setText(aux);
+                edtText.getText().clear();
+
         }
-
-
 
     }
 
@@ -75,7 +86,31 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         }
         if (ckbMaiusculo.isChecked()){
             textField.setText(textField.getText().toString().toUpperCase());
+        }else {
+            textField.setText(textField.getText().toString().toLowerCase());
+        }
 
+        if(ckbNegrito.isChecked() && ckbItalico.isChecked()){
+            textField.setTypeface(null, Typeface.BOLD_ITALIC);
+        }
+
+        if(rdbRoxo.isChecked()){
+            rdbLaranja.setChecked(false);
+            rdbCiano.setChecked(false);
+            textField.setTextColor(Color.parseColor("#808080"));
+            textField.setTextColor(Color.parseColor("#6a0dad"));
+        }
+        if(rdbLaranja.isChecked()){
+            rdbRoxo.setChecked(false);
+            rdbCiano.setChecked(false);
+            textField.setTextColor(Color.parseColor("#808080"));
+            textField.setTextColor(Color.parseColor("#ff9900"));
+        }
+        if(rdbCiano.isChecked()){
+            rdbRoxo.setChecked(false);
+            rdbLaranja.setChecked(false);
+            textField.setTextColor(Color.parseColor("#808080"));
+            textField.setTextColor(Color.parseColor("#00FFFF"));
         }
 
 
